a75700da7c03c7d748d12d2233ca112f040acd73
Jayson <jaysonsoo21@gmail.com>
2026-05-18 03:13:46 +0800

    Week 0: started lecture examples

---
+++ cs50p/week0/lecture_examples.py
@@ -0,0 +1,12 @@
+# Ask user for their name 
+name = input ("What is your name?").strip().title()
+
+#Split user's name into first and last name
+first, last = name.split(" ")
+
+#Say hello to the user
+print (f"hello, {first}")
+
+
+
+
